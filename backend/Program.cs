db38ed84f8d70674d6db77cca103cc509df82c64
Edgar Cunha <renatolfsantos@gmail.com>
2026-08-28 17:33:53 -0300

    pdianspdqnodpnqw

--- backend/Program.cs
+++ backend/Program.cs
@@ -10,12 +10,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins(
-                "http://localhost:5173",
-                "https://corinthians-website.vercel.app"
-            )
+            .AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod();
+
     });
 });
 
